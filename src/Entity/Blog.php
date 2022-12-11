9d7d863f4d09cbda90af241ecd974921d81444a7
Martijn van Sliedregt <>
2022-12-11 23:25:42 +0100

    Code cleanup

--- src/Entity/Blog.php
+++ src/Entity/Blog.php
@@ -3,16 +3,13 @@
 namespace App\Entity;
 
 use App\Repository\BlogRepository;
-use App\Service\MailManager;
-use DateTime;
 use DateTimeInterface;
-use DateTimeZone;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Validator\Constraints as Assert;
+
 #[ORM\HasLifecycleCallbacks]
 #[ORM\Entity(repositoryClass: BlogRepository::class)]
 class Blog
@@ -199,19 +196,4 @@ class Blog
 
         return $this;
     }
-
-
-/*
-    #[ORM\PrePersist]
-    public function sendEmail(): void
-    {
-        $mailManager = new MailManager();
-        try {
-            $mailManager->blogPublished($this);
-        } catch (TransportExceptionInterface $e) {
-            $this->logger->error("Blog published mail cannot be sent", [
-                'message' => $e->getMessage(),
-            ]);
-        }
-    }*/
 }
